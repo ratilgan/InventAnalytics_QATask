a6063210cd388bd07b5cd6837529349b3557c69f
ratilgan <ekserjetik@gmail.com>
2023-09-27 02:44:22 +0300

    minor updates

--- src/test/java/com/demoQA/pages/BasePage.java
+++ src/test/java/com/demoQA/pages/BasePage.java
@@ -1,4 +1,13 @@
 package com.demoQA.pages;
 
+import com.demoQA.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
 }
--- src/test/java/com/demoQA/pages/LandingPage.java
+++ src/test/java/com/demoQA/pages/LandingPage.java
@@ -1,4 +1,12 @@
 package com.demoQA.pages;
 
-public class LandingPage extends BasePage{
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LandingPage extends BasePage {
+
+    @FindBy(id = "login")
+    public WebElement loginButton;
+
+
 }
