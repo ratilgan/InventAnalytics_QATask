ac381bac568267bde4c2ddca9980f1c8c382c2b6
ratilgan <ekserjetik@gmail.com>
2023-09-26 21:16:54 +0300

    Hooks added

---
+++ src/test/java/com/demoQA/stepDefinitions/Hooks.java
@@ -0,0 +1,29 @@
+package com.demoQA.stepDefinitions;
+
+import com.demoQA.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before
+    public void setup() {
+        Driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.get().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "screenshot");
+        }
+        Driver.closeDriver();
+    }
+
+}
