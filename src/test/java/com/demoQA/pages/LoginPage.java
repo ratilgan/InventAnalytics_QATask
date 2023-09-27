35a51a60a9b47acba6242439c6a8ea25242e83f6
ratilgan <ekserjetik@gmail.com>
2023-09-27 19:51:48 +0300

    LoginPage is added

--- src/test/java/com/demoQA/pages/LoginPage.java
+++ src/test/java/com/demoQA/pages/LoginPage.java
@@ -1,4 +1,20 @@
 package com.demoQA.pages;
 
+import com.demoQA.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage extends BasePage{
+
+    @FindBy(id = "userName")
+    public WebElement usernameInputBox;
+
+    @FindBy(id = "password")
+    public WebElement passwordInputBox;
+
+    public void login(String username, String password){
+        usernameInputBox.sendKeys(ConfigurationReader.get(username));
+        passwordInputBox.sendKeys(ConfigurationReader.get(password), Keys.ENTER);
+    }
 }
