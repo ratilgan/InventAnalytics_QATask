1a8b86dbc1c63ba8219887c91407c74d00b78011
ratilgan <ekserjetik@gmail.com>
2023-09-27 19:52:10 +0300

    BasePage is added

--- src/test/java/com/demoQA/pages/BasePage.java
+++ src/test/java/com/demoQA/pages/BasePage.java
@@ -1,6 +1,10 @@
 package com.demoQA.pages;
 
 import com.demoQA.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
@@ -9,5 +13,16 @@ public class BasePage {
         PageFactory.initElements(Driver.get(), this);
     }
 
+    @FindBy(xpath = "(//li[@id='item-3'])[5]//span")
+    public WebElement profilePageLink;
 
+    public void closeGoogleAdIframe(){
+        Driver.get().switchTo().frame(Driver.get().findElement(By.xpath("//iframe[starts-with(@id,'google_ads_iframe')]")));
+        Driver.get().findElement(By.id("cbb")).click();
+        Driver.get().switchTo().parentFrame();
+    }
+
+    public static void scrollToElement(WebElement elementToScroll) {
+        ((JavascriptExecutor) Driver.get()).executeScript("arguments[0].scrollIntoView(true);", elementToScroll);
+    }
 }
