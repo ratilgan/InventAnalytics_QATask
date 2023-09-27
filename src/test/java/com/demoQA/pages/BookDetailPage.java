d69f3a22a11ccc5a097b2ec767984228518452f4
ratilgan <ekserjetik@gmail.com>
2023-09-27 19:51:32 +0300

    LandingPage is added

--- src/test/java/com/demoQA/pages/BookDetailPage.java
+++ src/test/java/com/demoQA/pages/BookDetailPage.java
@@ -1,4 +1,36 @@
 package com.demoQA.pages;
 
+import com.demoQA.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BookDetailPage extends BasePage {
+
+    WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(10));
+
+    @FindBy(css = ".text-right #addNewRecordButton")
+    public WebElement addToYourCollectionButton;
+
+    public void assertTheSuccessMessage(String expectedMessage){
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = Driver.get().switchTo().alert();
+        Assert.assertEquals("Wrong message is retrieved!", expectedMessage, alert.getText());
+        alert.accept();
+    }
+
+    public void clickAddToCollectionButton(){
+        scrollToElement(addToYourCollectionButton);
+        addToYourCollectionButton.click();
+    }
+
+    public void clickProfilePageLink(){
+        scrollToElement(addToYourCollectionButton);
+        profilePageLink.click();
+    }
 }
--- src/test/java/com/demoQA/pages/LandingPage.java
+++ src/test/java/com/demoQA/pages/LandingPage.java
@@ -1,5 +1,9 @@
 package com.demoQA.pages;
 
+import com.demoQA.utilities.ConfigurationReader;
+import com.demoQA.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -8,5 +12,26 @@ public class LandingPage extends BasePage {
     @FindBy(id = "login")
     public WebElement loginButton;
 
+    @FindBy(id = "userName-value")
+    public WebElement actualUsername;
+
+    @FindBy(id = "searchBox")
+    public WebElement searchInputBox;
+
+    @FindBy(css = "span>a")
+    public WebElement actualListedBookName;
+
+    public void verifyTheUserName(String expectedUserName){
+        Assert.assertEquals("The username is not matched!", ConfigurationReader.get(expectedUserName), actualUsername.getText());
+    }
+
+    public void verifyTheListedBookName(String expectedBookName){
+        scrollToElement(actualListedBookName);
+        Assert.assertEquals("The selected book is not listed", expectedBookName, actualListedBookName.getText());
+    }
+
+    public void clickOnTheBook(String bookName){
+        Driver.get().findElement(By.linkText(bookName)).click();
+    }
 
 }
